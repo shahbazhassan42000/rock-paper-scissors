a0c9bc53aba29b1649f20d9599e1ad8297ef54ea
Shahbaz Hassan <shahbazhassan42000@gmail.com>
2021-09-04 02:16:36 +0500

    setting configuration

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded",function (){
+    function computerPlay() {
+        const sides={
+          rock:"Rock",
 
-   
+        };
+        alert(sides.one);
+    }
+    document.getElementById("btn").addEventListener('click',computerPlay);
+
 });
